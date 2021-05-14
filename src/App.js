40aa430517ec9e1ec2528b579b61859921b99693
afraefatuaki <afrae@elfatuaki.com>
2021-05-14 14:57:13 +0200

    added arrow/buttons/fields components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import arrow from './components/arrow';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <arrow />
     </div>
   );
 }
--- src/components/fields/index.js
+++ src/components/fields/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+const fields = () => {
+    return (
+        <form action="/" className="logIn-form">
+            <input type="text" name="" value="" className="searchField"></input>
+            <input type="text" placeholder="enter Username" name="uname" className="logInField" required></input>
+            <input type="password" placeholder="enter Password" name="psw" className="passwordField" required></input>
+            <input type="submit" value="Submit"></input>
+        </form>
+    );
+}
+
+export default fields;
