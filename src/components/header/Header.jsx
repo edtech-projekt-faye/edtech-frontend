e7437910d7a63bd8d1e69fb622ff682f5c0912a1
ferniemann <ferdinand.niemann@gmail.com>
2021-05-15 12:42:39 +0200

    Card View optimized

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import './style.css';
+
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <div className="logo">
+                <img src="/favicon.jpg" alt="Logo edtech" />
+            </div>
+        </header>
+    )
+}
+
+export default Header
