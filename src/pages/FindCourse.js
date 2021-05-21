2139a80e1fb719a73a7d2289574bcf5cd080db43
ferniemann <ferdinand.niemann@gmail.com>
2021-05-21 15:58:26 +0200

    Minor changes in Splash Screen

--- src/pages/FindCourse.js
+++ src/pages/FindCourse.js
@@ -5,7 +5,7 @@ const FindCourse = () => {
         <div className="slider-page">
             <img className="slider-img" src="images/FindCourse.png" alt="intro" />
             <h2>Find a course for you</h2>
-            <p>Quarantine is the perfect time to spend your day learning something new, from anywhere</p>
+            <p>Learn web development with our courses. The best thing about it: It is completely free!</p>
         </div>
     );
 }
--- src/pages/LearnAnytime.js
+++ src/pages/LearnAnytime.js
@@ -4,7 +4,7 @@ const LearnAnytime = () => {
     return (
         <div className="slider-page">
             <img className="slider-img" src="images/learnAnytime.png" alt="intro" />
-            <h2>Learn anytime and anywhere</h2>
+            <h2>Learn anytime, anywhere</h2>
             <p>Quarantine is the perfect time to spend your day learning something new, from anywhere</p>
         </div>
     );
